7208d8d9af2b0235063b377d81d69adbbee70071
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-18 16:21:27 +0300

    Add solution for Metric Converter

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Metric Converter/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Metric_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string inputUnit = Console.ReadLine();
+            string outputUnit = Console.ReadLine();
+            switch (inputUnit)
+            {
+                case "m":
+                    break;
+                case "mm":
+                    number /= 1000;
+                    break;
+                case "cm":
+                    number /= 100;
+                    break;
+                case "mi":
+                    number /= 0.000621371192;
+                    break;
+                case "in":
+                    number /= 39.3700787;
+                    break;
+                case "km":
+                    number /= 0.001;
+                    break;
+                case "ft":
+                    number /= 3.2808399;
+                    break;
+                case "yd":
+                    number /= 1.0936133;
+                    break;
+            }
+
+            switch (outputUnit)
+            {
+                case "m":
+                    break;
+                case "mm":
+                    number *= 1000;
+                    break;
+                case "cm":
+                    number *= 100;
+                    break;
+                case "mi":
+                    number *= 0.000621371192;
+                    break;
+                case "in":
+                    number *= 39.3700787;
+                    break;
+                case "km":
+                    number *= 0.001;
+                    break;
+                case "ft":
+                    number *= 3.2808399;
+                    break;
+                case "yd":
+                    number *= 1.0936133;
+                    break;
+            }
+            Console.WriteLine(number + " " + outputUnit);
+        }
+    }
+}
---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Sum Seconds/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time1 = int.Parse(Console.ReadLine());
+            int time2 = int.Parse(Console.ReadLine());
+            int time3 = int.Parse(Console.ReadLine());
+            int minutes = 0;
+            int seconds = 0;
+            seconds = time1 + time2 + time3;
+            minutes = seconds / 60;
+            seconds = seconds - minutes * 60;
+            string secs = seconds < 10 ? "0" + seconds.ToString() : seconds.ToString();
+            Console.WriteLine(minutes.ToString()+":"+secs);
+        }
+    }
+}
