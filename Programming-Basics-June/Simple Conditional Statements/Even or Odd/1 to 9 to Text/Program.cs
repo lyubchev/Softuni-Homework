e46161f03526821ed3e261ecc3cff3bf029c063a
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-18 15:49:25 +0300

    Add solution for 1 to 9 to text

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/1 to 9 to Text/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1_to_9_to_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("one"); break;
+                case "2":
+                    Console.WriteLine("two"); break;
+                case "3":
+                    Console.WriteLine("three"); break;
+                case "4":
+                    Console.WriteLine("four"); break;
+                case "5":
+                    Console.WriteLine("five"); break;
+                case "6":
+                    Console.WriteLine("six"); break;
+                case "7":
+                    Console.WriteLine("seven"); break;
+                case "8":
+                    Console.WriteLine("eight"); break;
+                case "9":
+                    Console.WriteLine("nine"); break;
+                default:
+                    Console.WriteLine("number too big"); break;
+            }
+        }
+    }
+}
---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Number 1...9 to Text/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Number_1..._9_to_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
