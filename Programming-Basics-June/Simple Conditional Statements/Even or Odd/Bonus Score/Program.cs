4ce61eb618416198d674d6a23210cd93ee14fdbe
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-18 16:06:05 +0300

    Add solution for Bonus Score

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Bonus Score/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bonus_Score
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double score = double.Parse(Console.ReadLine());
+            double bonus = 0;
+            bonus = score % 2 == 0 ? 1 : bonus;
+            bonus = score % 10 == 5 ? 2 : bonus;
+            if (score<=100)
+            {
+                bonus += 5;
+            }
+            if (score>100 && score <=1000)
+            {
+                bonus += 0.2 * score;
+            }
+            if(score>1000)
+            {
+                bonus += 0.1 * score;
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(bonus+score);
+        }
+    }
+}
