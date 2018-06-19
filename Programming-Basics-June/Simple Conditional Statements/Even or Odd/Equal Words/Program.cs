3a5ced1f4ae44b35e79da89c07eacdf156d1558a
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-19 17:06:39 +0300

    Add solution for Equal Words

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Equal Words/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace Equal_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            string input2 = Console.ReadLine().ToLower();
+            if (input.Equals(input2))
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+        }
+    }
+}
