4cfdd3d0057fa7cc7ec5cdd0a03979a3ef48a4c2
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-19 16:52:36 +0300

    Add solution for Number between 100 200

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Number between 100 200/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Number_between_100_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            if (input < 100) Console.WriteLine("Less than 100");
+            if (input >= 100 && input <= 200) Console.WriteLine("Between 100 and 200");
+            if (input > 200) Console.WriteLine("Greater than 200");
+        }
+    }
+}
