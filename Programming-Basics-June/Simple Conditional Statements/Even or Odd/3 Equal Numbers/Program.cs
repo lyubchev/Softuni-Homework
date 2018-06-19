e8cd293d177e984b09ced25c50121e47cd674c1e
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-19 17:41:49 +0300

    Add solution for 3 Equal Numbers

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/3 Equal Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3_Equal_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input1 = double.Parse(Console.ReadLine());
+            double input2 = double.Parse(Console.ReadLine());
+            double input3 = double.Parse(Console.ReadLine());
+
+            if (input1==input2&&input2==input3)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
