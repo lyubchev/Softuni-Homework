ae2164a453eeadb1fa0b4aeab50ff743e8072021
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-19 17:20:30 +0300

    Add solution for Speed Info

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Speed Info/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Speed_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double speed = double.Parse(Console.ReadLine());
+            // MEME CONDITION
+            Console.WriteLine(speed <= 10 ? "slow" : speed > 10 && speed <= 50 ? "average" : speed > 50 && speed <= 150 ? "fast" : speed > 150 && speed <= 1000 ? "ultra fast" : "extremely fast");
+        }
+    }
+}
