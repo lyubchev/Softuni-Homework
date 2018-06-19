bdd872d4b4a7ba5d2826cb0ef89e5ea6799341fe
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-19 17:27:49 +0300

    Add solution for Area of Figures

---
+++ Programming-Basics-June/Simple Conditional Statements/Even or Odd/Area of Figures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            switch (figure)
+            {
+                case "square":
+                    double a = double.Parse(Console.ReadLine());
+                    Console.WriteLine(a * a);
+                    
+                    break;
+                case "rectangle":
+                    a = double.Parse(Console.ReadLine());
+                    double b = double.Parse(Console.ReadLine());
+                    Console.WriteLine(a * b);
+                    break;
+                case "circle":
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine(Math.PI * a*a);
+                    break;
+                case "triangle":
+                    a = double.Parse(Console.ReadLine());
+                    b = double.Parse(Console.ReadLine());
+                    Console.WriteLine((a*b)/2);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
