fe2110ae73f872f263142d8fbf13f6b5580c8177
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-30 20:35:54 +0300

    Add solution to Graduation

---
+++ Programming-Basics-June/While-Loop/Ex/Graduation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double avg = 0;
+            double grade = 6;
+            int currentGrade = 1;
+            while (currentGrade<=12)
+            {
+                grade = double.Parse(Console.ReadLine());
+                if (grade >= 4.0) avg += grade;
+                else currentGrade--;
+                currentGrade++;
+            }
+            avg /= 12.0;
+            if (avg >= 4.0)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {avg:F2}");
+            }
+        }
+    }
+}
