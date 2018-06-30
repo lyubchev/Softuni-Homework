630b132c0472a5ad9a4a27b10c6d6c0d919785b1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-06-30 20:18:21 +0300

    Add solution to Number in Range

---
+++ Programming-Basics-June/While-Loop/Ex/Ex/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex
+{
+    class Number_in_Range
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            while (range < 1 || range > 100)
+            {
+                Console.WriteLine("Invalid number!");
+                range = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The number is: " + range);
+        }
+    }
+}
