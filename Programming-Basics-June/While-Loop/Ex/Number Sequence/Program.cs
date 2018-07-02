19c8053a9e12dcc7ada2406f40aa460fee2fdacf
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-02 13:23:54 +0300

    Add solution to Number Sequence

---
+++ Programming-Basics-June/While-Loop/Ex/Number Sequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Number_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+
+                if (int.Parse(input) >= max)
+                {
+                    max = int.Parse(input);
+                }
+                if (int.Parse(input) <= min)
+                {
+                    min = int.Parse(input);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Max number: "+ max);
+            Console.WriteLine("Min number: " + min);
+        }
+    }
+}
