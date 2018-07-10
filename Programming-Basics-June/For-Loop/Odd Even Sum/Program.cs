56d701171ba3a8ed5ce3a58b1af6c8832046c5a1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-10 14:30:11 +0300

    Add solution to Odd Even Sum

---
+++ Programming-Basics-June/For-Loop/Odd Even Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int even = 0, odd = 0;
+            int diff = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    even += int.Parse(Console.ReadLine());
+                else
+                    odd += int.Parse(Console.ReadLine());
+            }
+            diff = Math.Abs(even - odd);
+            if (even == odd)
+                Console.WriteLine("Yes, sum = {0}", even);
+            else
+                Console.WriteLine("No, diff = {0}", diff);
+        }
+    }
+}
