72a489ab1260bdb0a2f17d54dc5c41393f2619ae
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-10 14:12:06 +0300

    Add solution to Min Number

---
+++ Programming-Basics-June/For-Loop/Min Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            int value = 0;
+            for (int i = 0; i < n; i++)
+            {
+                value = int.Parse(Console.ReadLine());
+                if (value <= min)
+                {
+                    min = value;
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
