ea56dde8f187a8a407832d9f8f95dae04d7efc25
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-10 14:11:04 +0300

    Add solution to Max Number

---
+++ Programming-Basics-June/For-Loop/Max Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int value = 0;
+            for (int i = 0; i < n; i++)
+            {
+                value = int.Parse(Console.ReadLine());
+                if (value>=max)
+                {
+                    max = value;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
