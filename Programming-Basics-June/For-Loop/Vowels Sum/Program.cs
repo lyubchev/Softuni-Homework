326786c178e78b92e6cd25c948e4dbb1cb20785c
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-10 14:40:12 +0300

    Add solution to Vowels Sum

---
+++ Programming-Basics-June/For-Loop/Vowels Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>()
+            {
+                { 'a', 1 },
+                { 'e', 2 },
+                { 'i', 3 },
+                { 'o', 4 },
+                { 'u', 5 },
+            };
+            int sum = 0;
+            string input = Console.ReadLine();
+            foreach (char letter in input)
+            {
+                if (map.ContainsKey(letter)) sum += map[letter];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
