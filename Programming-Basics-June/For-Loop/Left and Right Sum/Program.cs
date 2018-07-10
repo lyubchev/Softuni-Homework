051c0b218a4b45bae030381dcd1eaaa9caee24e8
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-10 14:20:49 +0300

    Add solution to Left and Right Sum

---
+++ Programming-Basics-June/For-Loop/Left and Right Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int left = 0, right = 0;
+            int diff = 0;
+            for (int i = 0; i < n * 2; i++)
+            {
+                if (i < n)
+                {
+                    left += int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    right += int.Parse(Console.ReadLine());
+                }
+            }
+            diff = Math.Abs(left - right);
+            if (diff == 0) Console.WriteLine("Yes, sum = {0}",left);
+            else Console.WriteLine("No, diff = {0}", diff);
+        }
+    }
+}
