4581e5a26a39be71253afb65294a92117c184a4d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-10 14:03:41 +0300

    Add solution to Latin Letters

---
+++ Programming-Basics-June/For-Loop/Latin Letters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i <= 97+25; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+        }
+    }
+}
