1b001262467c90fce4c39ecc440b846cdb19bdd7
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-16 18:04:24 +0300

    Add solution to Numbers 1-N with step 3

---
+++ Programming-Basics-June/Nested Loops/Numbers 1N with step 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Numbers_N1_with_step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
