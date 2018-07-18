448025a8aea705ae4a3565226df651fc2efe424d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-18 13:43:37 +0300

    Add solution to Travelling

---
+++ Programming-Basics-June/Nested Loops/Travelling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Travelling {
+    class Program {
+        static void Main (string[] args) {
+
+            while (true) {
+                string goal = Console.ReadLine ();
+                if (goal == "End") break;
+                double minBudget = double.Parse (Console.ReadLine ());
+                double saved = 0;
+                while (saved < minBudget) {
+                    saved += double.Parse (Console.ReadLine ());
+                }
+                Console.WriteLine ($"Going to {goal}!");
+            }
+        }
+    }
+}
