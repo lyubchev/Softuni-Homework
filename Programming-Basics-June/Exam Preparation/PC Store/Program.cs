ef84f416a5b9f1e3d3da9b4e0ce04d0f9df92733
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-24 21:26:10 +0300

    Add solution to PC Store

---
+++ Programming-Basics-June/Exam Preparation/PC Store/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PC_STORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double dollar = 1.57;
+            double processor = double.Parse(Console.ReadLine());
+            double videoCard = double.Parse(Console.ReadLine());
+            double ram = double.Parse(Console.ReadLine());
+            int ramAmount = int.Parse(Console.ReadLine());
+            double discount = 1 - double.Parse(Console.ReadLine());
+            double total = (discount * videoCard) + (discount * processor) + (ram * ramAmount);
+            total *= dollar;
+            System.Console.WriteLine($"Money needed - {total:F2} leva.");
+        }
+    }
+}
