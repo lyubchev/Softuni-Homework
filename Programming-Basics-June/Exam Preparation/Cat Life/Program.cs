b71d8c04f8cb9f969a7f252c7f1aeaf83da138c1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-27 16:26:36 +0300

    Add solution to Cat Life

---
+++ Programming-Basics-June/Exam Preparation/Cat Life/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Cat_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string breed = Console.ReadLine();
+            string sex = Console.ReadLine();
+            int months = 0;
+            switch (breed)
+            {
+                case "British Shorthair":
+                    months = 13;
+                    break;
+                case "Siamese":
+                    months = 15;
+                    break;    
+                case "Persian":
+                    months = 14;
+                    break;  
+                case "Ragdoll":
+                    months = 16;
+                    break;
+                case "American Shorthair":
+                    months = 12;
+                    break;
+                case "Siberian":
+                    months = 11;
+                    break;
+                default:
+                Console.WriteLine($"{breed} is invalid cat!");
+                return;
+                break;
+            }
+            if(sex == "f") months++;
+            months *= 12;
+            months = Convert.ToInt32(Math.Floor(months/6.0));
+            Console.WriteLine($"{months} cat months");
+        }
+    }
+}
---
+++ Programming-Basics-June/Nested Loops/Number Pyramid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Number_Pyramid {
+    class Program {
+        static void Main (string[] args) {
+            int n = int.Parse (Console.ReadLine ());
+            int num = 1;
+            for (int row = 1; row <= n; row++) {
+                for (int col = 1; col <= row; col++) {
+                    if (col > 1) { Console.Write (" "); }
+                    Console.Write (num);
+                    num++;
+                    if (num > n) { break; }
+                }
+                Console.WriteLine ();
+                if (num > n) break;
+            }
+
+        }
+    }
+}
