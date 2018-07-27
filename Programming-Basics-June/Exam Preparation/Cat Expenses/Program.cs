d528ac9b3e7d098f1691b30d4ff5295554a567d0
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-27 16:54:31 +0300

    Add solution to Cat Expenses

---
+++ Programming-Basics-June/Exam Preparation/Cat Expenses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cat_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double food = double.Parse(Console.ReadLine())*1000.0;
+            string command = Console.ReadLine();
+            
+            while (command != "Adopted")
+            {
+                food -= double.Parse(command);
+                command = Console.ReadLine();
+            }
+            if(food >= 0)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {food} grams.");
+            }
+            else
+            {
+                food *= -1;
+                Console.WriteLine($"Food is not enough. You need {food} grams more.");
+            }
+        }
+    }
+}
