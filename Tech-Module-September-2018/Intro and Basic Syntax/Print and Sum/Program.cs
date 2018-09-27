14cf46473915eef22c7cfcb64f334fec51e29bae
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-27 17:35:34 +0300

    Add solution to Print and Sum

---
+++ Tech-Module-September-2018/Intro and Basic Syntax/Print and Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Print_and_Sum {
+    class Program {
+        static void Main (string[] args) {
+            int start = int.Parse (Console.ReadLine ());
+            int end = int.Parse (Console.ReadLine ());
+            int sum = 0;
+
+            for (int i = start; i <= end; i++) {
+                sum += i;
+                if (i >= start && i < end) {
+                    Console.Write (i + " ");
+                } else if (i == end) {
+                    Console.Write (i);
+                }
+            }
+            Console.WriteLine ($"\nSum: {sum}");
+        }
+    }
+}
