477a367911155163bca3523b4339431ff6678ab7
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-27 16:51:28 +0300

    Create Tech-Module-September-2018 dir, Add solution to Ages

---
+++ Tech-Module-September-2018/Intro and Basic Syntax/Ages/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ages {
+    class Program {
+        static void Main (string[] args) {
+            int age = int.Parse (Console.ReadLine ());
+
+            if (age >= 0 && age <= 2) {
+                Console.WriteLine ("baby");
+                return;
+            }
+
+            if (age >= 3 && age <= 13) {
+                Console.WriteLine ("child");
+                return;
+            }
+
+            if (age >= 14 && age <= 19) {
+                Console.WriteLine ("teenager");
+                return;
+            }
+
+            if (age >= 20 && age <= 65) {
+                Console.WriteLine ("adult");
+                return;
+            }
+
+            if (age >= 66) {
+                Console.WriteLine ("elder");
+                return;
+            }
+
+        }
+    }
+}
