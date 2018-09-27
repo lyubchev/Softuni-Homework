f88237062cfb59760ae1b5ca45b3205130d072aa
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-27 18:07:55 +0300

    Add solution to Login

---
+++ Tech-Module-September-2018/Intro and Basic Syntax/Login/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Login {
+    class Program {
+        static void Main (string[] args) {
+
+            string username = Console.ReadLine ();
+            string password = GeneratePassword (username);
+            string input = Console.ReadLine ();
+            int badLoginAttempts = 0;
+            while (input != password) {
+                badLoginAttempts++;
+                if (badLoginAttempts >= 4) {
+                    break;
+                }
+                Console.WriteLine ("Incorrect password. Try again.");
+                input = Console.ReadLine ();
+            }
+            Console.WriteLine (badLoginAttempts <= 3 ? $"User {username} logged in." : $"User {username} blocked!");
+        }
+
+        static string GeneratePassword (string username) {
+
+            System.Text.StringBuilder password = new System.Text.StringBuilder ();
+            for (int i = 0; i < username.Length; i++) {
+                password.Append (username[username.Length - i - 1]);
+            }
+            return password.ToString ();
+        }
+    }
+}
