c0a25d2a4de770f77fd2359f9d912c56a37d1c17
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-28 18:26:14 +0300

    Add solution to Strong Number

---
+++ Tech-Module-September-2018/Intro and Basic Syntax/Strong Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            if(IsStrongNumber(number)) {
+                Console.WriteLine("yes");
+            } else {
+                Console.WriteLine("no");
+            }
+        }
+
+        static bool IsStrongNumber(string number) {
+
+            int total = 0;
+
+            for (int i = 0; i < number.Length; i++) {
+                total += factorial(int.Parse(number[i].ToString()));
+            }
+            return (total == int.Parse(number)) ? true: false;
+        }
+
+        static int factorial(int n) => (n <= 1) ? 1 : n * factorial(n-1);
+    }
+}
