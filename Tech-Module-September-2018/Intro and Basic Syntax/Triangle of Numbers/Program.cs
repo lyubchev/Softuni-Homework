32eb6f6fd6167fdcff504cf685054759a3001403
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-29 19:49:50 +0300

    Add solution to Triangle of Numbers

---
+++ Tech-Module-September-2018/Intro and Basic Syntax/Triangle of Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Triangle_of_Numbers {
+    class Program {
+        static void Main (string[] args) {
+            int n = int.Parse (Console.ReadLine ());
+            if (n >= 1 && n <= 20) {
+
+                for (int i = 1; i <= n; i++) {
+
+                    for (int j = 0; j < i; j++) {
+                        Console.Write (i + " ");
+                    }
+                    Console.WriteLine ();
+                }
+            }
+        }
+    }
+}
