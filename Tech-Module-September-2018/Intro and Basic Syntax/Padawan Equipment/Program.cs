324a487451e6cdccc3dcd42110959f2937d717d9
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-09-30 16:11:10 +0300

    Add solution to Padawan Equipment

---
+++ Tech-Module-September-2018/Intro and Basic Syntax/Padawan Equipment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Padawan_Equipment {
+    class Program {
+        static void Main (string[] args) {
+            decimal balance = decimal.Parse (Console.ReadLine ());
+            int studentsCount = int.Parse (Console.ReadLine ());
+            decimal lightsaberPrice = decimal.Parse (Console.ReadLine ());
+            decimal robePrice = decimal.Parse (Console.ReadLine ());
+            decimal beltPrice = decimal.Parse (Console.ReadLine ());
+
+            // Ivan Cho needs 10% more lightsabers, because sometimes they brake
+            int lightsabersCount = (int) Math.Ceiling (1.1 * studentsCount);
+            int robesCount = studentsCount;
+            int beltsCount = studentsCount - (studentsCount / 6);
+
+            decimal total = lightsabersCount * lightsaberPrice + robesCount * robePrice + beltsCount * beltPrice;
+
+            if (total <= balance) {
+                Console.WriteLine ($"The money is enough - it would cost {total:F2}lv.");
+            } else {
+                Console.WriteLine ($"Ivan Cho will need {(total - balance/*  */):F2}lv more.");
+            }
+
+        }
+    }
+}
