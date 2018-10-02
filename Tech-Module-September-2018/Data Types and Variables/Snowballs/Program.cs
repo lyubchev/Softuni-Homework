a969fdef7ac6e0101d5eefeba95652c555249345
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-02 18:39:54 +0300

    Add solution to Snowballs

---
+++ Tech-Module-September-2018/Data Types and Variables/Snowballs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+namespace Snowballs {
+    class Program {
+        static void Main (string[] args) {
+            int snowballs = int.Parse (Console.ReadLine ());
+            int bestSnowballSnow = 0;
+            int bestSnowballTime = 0;
+            int bestSnowballQuality = 0;
+            BigInteger bestSnowballValue = 0;
+            for (int i = 0; i < snowballs; i++) {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+                if ( snowballValue >= bestSnowballValue) {
+                    bestSnowballValue = snowballValue;
+                    bestSnowballSnow = snowballSnow;
+                    bestSnowballTime = snowballTime;
+                    bestSnowballQuality = snowballQuality;
+                }
+            }
+            Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestSnowballValue} ({bestSnowballQuality})");
+        }
+    }
+}
