85586adb8a93d9ff76143a95d5df1cd57afda35c
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-02 17:22:34 +0300

    Add solution to Chars to String

---
+++ Tech-Module-September-2018/Data Types and Variables/Chars to String/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            a += Console.ReadLine();
+            a += Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
