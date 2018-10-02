9ac29af24a88542b1b5e1c3fc8bbdc70f2ef8130
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-02 18:11:26 +0300

    Add solution to Pounds to Dollars

---
+++ Tech-Module-September-2018/Data Types and Variables/Pounds to Dollars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pounds_to_Dollars {
+    class Program {
+        static void Main (string[] args) {
+            const double britishPound = 1.31;
+            double britishPounds = double.Parse (Console.ReadLine());
+            double dollars = britishPounds * britishPound;
+            Console.WriteLine ($"{dollars:F3}");
+        }
+    }
+}
