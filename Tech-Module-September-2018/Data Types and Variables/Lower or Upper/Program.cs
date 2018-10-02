6e0cf4ed0707243a5f90d4d623537a2c989c9082
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-02 18:19:32 +0300

    Add solution to Lower or Upper

---
+++ Tech-Module-September-2018/Data Types and Variables/Lower or Upper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lower_or_Upper {
+    class Program {
+        static void Main (string[] args) {
+            string input = Console.ReadLine ();
+            if (input.ToUpper () == input) {
+                Console.WriteLine ("upper-case");
+            } else {
+                Console.WriteLine ("lower-case");
+            }
+        }
+    }
+}
