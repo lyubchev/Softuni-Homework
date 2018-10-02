fecc1a5cc2ae547eacd50d6c90f9a44dc4c7e89e
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-02 18:15:57 +0300

    Add solution to Reversed Chars

---
+++ Tech-Module-September-2018/Data Types and Variables/Reversed Chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reversed_Chars {
+    class Program {
+        static void Main (string[] args) {
+            string a = Console.ReadLine ();
+            a += Console.ReadLine ();
+            a += Console.ReadLine ();
+            a = ReverseString (a);
+            Console.WriteLine (a);
+        }
+
+        static string ReverseString (string a) {
+            System.Text.StringBuilder reversed = new System.Text.StringBuilder ();
+            for (int i = 0; i < a.Length; i++) {
+                reversed.Append (a[a.Length - i - 1] + " ");
+            }
+            return reversed.ToString ();
+        }
+    }
+}
