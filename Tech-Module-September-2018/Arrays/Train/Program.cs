f3ec1841e1714254fb5454d2728593ac586b47a5
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-11 20:46:14 +0300

    Add solution to Train

---
+++ Tech-Module-September-2018/Arrays/Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+               arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int arrSum = arr.Sum();
+            Console.WriteLine(string.Join(" ", arr));
+            Console.WriteLine(arrSum);
+        }
+    }
+}
