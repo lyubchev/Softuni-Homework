990f2450700649a5c259d60f2f19f1e861836c80
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-16 19:51:17 +0300

    Add solution to Zig-Zag Arrays

---
+++ Tech-Module-September-2018/Arrays/Zig-Zag Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n * 2];
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    arr1[i] = numbers[0];
+                    arr2[i] = numbers[1];
+                }
+                else
+                {
+                    arr1[i] = numbers[1];
+                    arr2[i] = numbers[0];
+                }
+            }
+            Console.WriteLine(string.Join(" ", arr1));
+            Console.WriteLine(string.Join(" ", arr2));
+        }
+    }
+}
