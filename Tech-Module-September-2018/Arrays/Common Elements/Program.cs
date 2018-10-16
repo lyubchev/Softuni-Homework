a9f6c5e67fa429c4cd02d8f45867463148c2e6ee
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-16 19:41:54 +0300

    Add solution to COmmon Elements

---
+++ Tech-Module-September-2018/Arrays/Common Elements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split();
+            string[] arr2 = Console.ReadLine().Split();
+            string commonElements = "";
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = 0; j < arr1.Length; j++)
+                {
+                    if (arr2[i] == arr1[j])
+                    {
+                        commonElements += arr2[i] + " ";
+                    }
+                }
+            }
+
+            Console.WriteLine(commonElements);
+        }
+    }
+}
--- Tech-Module-September-2018/Arrays/Train/Program.cs
+++ Tech-Module-September-2018/Arrays/Train/Program.cs
@@ -19,4 +19,27 @@ namespace Train
             Console.WriteLine(arrSum);
         }
     }
-}
+
+
+using System;
+using System.Linq;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+               arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int arrSum = arr.Sum();
+            Console.WriteLine(string.Join(" ", arr));
+            Console.WriteLine(arrSum);
+        }
+    }
+}}
