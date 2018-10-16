197db93e5b8c9b5edb4a851db8ce67b3f6ed2fe2
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-16 20:44:15 +0300

    Add solution to Equal Sum

---
+++ Tech-Module-September-2018/Arrays/Equal Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int leftSum = arr.Sum();
+            int rightSum = 0;
+            int index = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                rightSum += arr[i];
+
+                if (rightSum == leftSum)
+                {
+                    index = i;
+                }
+
+                leftSum -= arr[i];
+
+            }
+            Console.WriteLine(index > -1 ? index.ToString() : "no");
+        }
+    }
+}
