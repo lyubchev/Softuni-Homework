5bc740260bdf02c7b72c092041a8425bc390a936
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-16 20:06:16 +0300

    Add solution to array rotation

---
+++ Tech-Module-September-2018/Arrays/Array rotation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace Array_rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine()) % arr.Length;
+            int first;
+
+            for (int i = 0; i < rotations; i++)
+            {
+                first = arr[0];
+
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+
+                arr[arr.Length - 1] = first;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
