d3a916148ffe72be257b6c214f622aecbf646389
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-16 21:12:29 +0300

    Add solution to Max Sequence of Equal Elements

---
+++ Tech-Module-September-2018/Arrays/Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int current = 0;
+            int currentLength = 1;
+            int best = 0;
+            int bestLength = 1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                current = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (current == arr[j])
+                    {
+                        currentLength++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (currentLength > bestLength)
+                {
+                    best = current;
+                    bestLength = currentLength;
+                }
+                currentLength = 1;
+            }
+
+            if (bestLength == 1)
+            {
+                best = arr[0];
+            }
+
+            for (int i = 0; i < bestLength; i++)
+            {
+                Console.Write(best + " ");
+            }
+        }
+    }
+}
