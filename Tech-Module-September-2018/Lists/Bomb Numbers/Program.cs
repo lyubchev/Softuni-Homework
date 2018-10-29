7ea05ec94a24662ebea68280636ae0bbbff2f1f0
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-10-29 23:13:55 +0200

    Add solution to Bomb Numbers

---
+++ Tech-Module-September-2018/Lists/Bomb Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string bombSpecs = Console.ReadLine();
+            int bombTarget = int.Parse(bombSpecs.Split()[0]);
+            int bombPower = int.Parse(bombSpecs.Split()[1]);
+
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                if (numbers[i] == bombTarget)
+                {
+                    int start = Math.Max(0, i - bombPower);
+                    int end = Math.Min((2 * bombPower) + 1, numbers.Count() - start);
+                    numbers.RemoveRange(start, end);
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
