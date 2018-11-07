1a96956eeaf2ca28d0a4412b4c2a3a9499f5f8bb
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-11-07 22:35:26 +0200

    Add solution to Count Chars in a String

---
+++ Tech-Module-September-2018/Dictionaries, Lambda and LINQ/Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().Replace(" ", "");
+
+            var letters = new Dictionary<char, int>();
+
+            foreach (var letter in input)
+            {
+                if (letters.ContainsKey(letter))
+                {
+                    letters[letter]++;
+                }
+                else
+                {
+                    letters[letter] = 1;
+                }
+            }
+
+            foreach (var letter in letters)
+            {
+                Console.WriteLine($"{letter.Key} -> {letter.Value}");
+            }
+        }
+    }
+}
