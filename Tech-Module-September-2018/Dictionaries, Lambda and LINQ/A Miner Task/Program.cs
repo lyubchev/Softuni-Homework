c4a50e5d9b79a4aba75c21afc309c5aa0339c7df
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-11-07 23:18:53 +0200

    Add solution to A Miner Task

---
+++ Tech-Module-September-2018/Dictionaries, Lambda and LINQ/A Miner Task/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var resources = new Dictionary<string, int>();
+            string resource = "";
+            bool isResource = true;
+
+            while (input != "stop")
+            {
+                if (isResource)
+                {
+                    resource = input;
+                }
+                else
+                {
+                    if (resources.ContainsKey(resource))
+                    {
+                        resources[resource] += int.Parse(input);
+                    } else {
+                        resources[resource] = int.Parse(input);
+                    }
+                }
+
+                input = Console.ReadLine();
+                isResource = !isResource;
+            }
+
+            foreach (var item in resources)
+            {
+                
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
