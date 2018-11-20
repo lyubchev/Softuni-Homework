0c14bd55c4c795a02f89a8780eb5a5c460c8c19d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-11-20 18:48:48 +0200

    Add solution to Valid Usernames

---
+++ Tech-Module-September-2018/Objects And Classes/Articles/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Articles
+{
+    class Article
+    {
+        private string title;
+        private string content;
+        private string author;
+
+        public Article(string[] data)
+        {
+            this.title = data[0];
+            this.content = data[1];
+            this.author = data[2];
+        }
+
+        public void Edit(string newContent)
+        {
+            this.content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            this.author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            this.title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.title} - {this.content}: {this.author}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GOTTA SAVE THAT MEMORY 
+            string[] parameters = Console.ReadLine().Split(", ");
+
+            Article article = new Article(parameters);
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; n++)
+            {
+                string command = Console.ReadLine();
+
+                switch (command.Split(":")[0])
+                {
+                    case "Edit":
+                        article.Edit(command.Split()[1].TrimStart());
+                        break;
+                    case "ChangeAuthor":
+                        article.ChangeAuthor(command.Split()[1].TrimStart());
+                        break;
+                    case "Rename":
+                        article.Rename(command.Split()[1].TrimStart());
+                        break;
+                }
+            }
+
+            Console.WriteLine(article.ToString());
+        }
+    }
+}
---
+++ Tech-Module-September-2018/String and Text Processing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace String_and_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine()
+            .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+            
+            string pattern = @"^\b[A-Za-z0-9_-]{3,16}$";
+            Regex reg = new Regex(pattern);
+            var validInputs = new List<string>();
+
+            foreach (var input in inputs)
+            {
+                var match = reg.Match(input);
+
+                if (match.Value != "")
+                {
+                    validInputs.Add(match.Value);
+                }
+            }
+
+            Console.WriteLine(string.Join("\n", validInputs));
+        }
+    }
+}
