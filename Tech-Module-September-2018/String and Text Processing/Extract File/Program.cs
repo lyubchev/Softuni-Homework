85086619edf333f1f147a936869f58ee85998e4d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-11-20 20:08:03 +0200

    Add solution to Extract File

---
+++ Tech-Module-September-2018/String and Text Processing/Extract File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split("\\");
+
+            string[] file = path[path.Length - 1].Split(".");
+            string fileName = file[0];
+            string fileExtension = file[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
