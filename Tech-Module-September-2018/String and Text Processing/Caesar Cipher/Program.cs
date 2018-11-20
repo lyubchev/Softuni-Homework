71981620ee79a540ae632744c566ece60d461905
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-11-20 20:18:06 +0200

    Add solution to Caesar Cipher

---
+++ Tech-Module-September-2018/String and Text Processing/Caesar Cipher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string encryptedText = Encrypt(input);
+            Console.WriteLine(encryptedText);
+        }
+
+        static string Encrypt(string input)
+        {
+            string encryptedText = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                encryptedText += ((char)(input[i] + 3)).ToString();
+            }
+
+            return encryptedText;
+        }
+    }
+}
