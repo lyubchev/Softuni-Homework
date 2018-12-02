41403762bf3c0d6c003e5089d773f7392613ea40
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-12-02 21:17:46 +0200

    Add solution to Calculator MVC App

---
+++ Tech-Module-September-2018/Basic Web/CalculatorApp/Models/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Models
+{
+    public class Calculator
+    {
+        public double LeftOperand { get; set; }
+        public string Operator { get; set; }
+        public double RightOperand { get; set; }
+        public double Result { get; set; }
+
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+
+        public void CalculateResult()
+        {
+            switch (this.Operator)
+            {
+                case "+":
+                    this.Result = this.LeftOperand + this.RightOperand;
+                    break;
+                case "-":
+                    this.Result = this.LeftOperand - this.RightOperand;
+                    break;
+                case "*":
+                    this.Result = this.LeftOperand * this.RightOperand;
+                    break;
+                case "/":
+                    this.Result = this.LeftOperand / this.RightOperand;
+                    break;
+                case "^":
+                    this.Result = Math.Pow(this.LeftOperand, this.RightOperand);
+                    break;
+                default:
+                    this.Result = 0;
+                    break;
+            }
+        }
+    }
+}
